9dc09176cc8ab20ffd7b99f412a77fbc43d531c2
Nick <z.nick00@gmail.com>
2018-12-26 00:39:50 -0500

    Adds hero styles and section

--- src/public/index.php
+++ src/public/index.php
@@ -4,10 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
     <title>Hello World!</title>
 </head>
 <body>
-    Hello Worldzzz!
+    <section id="hero" class="hero">
+        <div class="container">
+            <h1 class="site-title">Nick Zou</h1>
+            <h2 class="site-subtitle">front end design and development</h2>
+        </div>
+    </section>
 </body>
 <script src="js/app.js"></script>
 </html>
