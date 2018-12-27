e6610ec9cb62b9881ead343e51f9ed912440d922
Nick <z.nick00@gmail.com>
2018-12-27 15:51:50 -0500

    Adds smooth scrolling

--- src/js/public/global/onLoad.js
+++ src/js/public/global/onLoad.js
@@ -1,5 +1,9 @@
 import loadingScreen from './functions/loadingScreen';
+import SmoothScroll from 'smooth-scroll';
 
 document.addEventListener('DOMContentLoaded', ()=> {
     loadingScreen();
+    new SmoothScroll('a[href*="#"]', {
+        speed: 300
+    });
 });
