68ad0ac12ccad13cc7e1b7fd64adf61f76fff356
Nick <z.nick00@gmail.com>
2018-12-27 02:38:38 -0500

    Adds about section template

---
+++ src/public/sections/about.php
@@ -0,0 +1,6 @@
+<section id="about" class="about jarallax">
+    <div id="about-info-panel" class="info-panel">
+        <h1>about</h1>
+    </div>
+    <div id="about-feature-panel" class="feature-panel">more stuff</div>
+</section>
