184a1df9e6a95dcfff31bce3a085392f1b1bd3cc
Nick <z.nick00@gmail.com>
2018-12-27 15:38:39 -0500

    Adds fullpage.js package and initiates

--- src/js/public/global/onLoad.js
+++ src/js/public/global/onLoad.js
@@ -1,5 +1,9 @@
 import loadingScreen from './functions/loadingScreen';
+import fullpage from 'fullpage.js';
 
 document.addEventListener('DOMContentLoaded', ()=> {
     loadingScreen();
+    new fullpage('#site-wrapper', {
+        autoScrolling: true
+    });
 });
