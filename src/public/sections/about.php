bd47ac977a2230f9028ca936903d4593098d1a0a
Nick <z.nick00@gmail.com>
2018-12-27 15:37:53 -0500

    Refactors about section class

--- src/public/sections/about.php
+++ src/public/sections/about.php
@@ -1,6 +1,8 @@
-<section id="about" class="about jarallax">
-    <div id="about-info-panel" class="info-panel">
-        <h1>about</h1>
+<section id="about" class="about section">
+    <div class="container full-width">
+        <div id="about-info-panel" class="info-panel">
+            <h1>about</h1>
+        </div>
+        <div id="about-feature-panel" class="feature-panel">more stuff</div>
     </div>
-    <div id="about-feature-panel" class="feature-panel">more stuff</div>
 </section>
