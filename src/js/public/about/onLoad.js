d67719b7aa874ae39d77171e7a90735f790b02e6
Nick <z.nick00@gmail.com>
2018-12-28 22:39:47 -0500

    Refactors section functions and destructures the data object

--- src/js/public/about/onLoad.js
+++ src/js/public/about/onLoad.js
@@ -5,19 +5,20 @@ document.addEventListener('DOMContentLoaded', ()=> {
     fetch('http://localhost/portfolio2019/admin//wp-json/wp/v2/pages/19')
     .then((response)=> {return response.json();})
     .then((data)=> {
+        const {title, content, acf} = data;
         //Temporary output to be able to see the data being returned
         //console.log(data);
         //sets About Section title
-        aboutTitle.innerHTML = data.title.rendered;
+        aboutTitle.innerHTML = title.rendered;
         //sets About Section content
-        aboutText.innerHTML = data.content.rendered;
-        aboutBackgroundImage.srcset = data.acf.background_image.sizes["hero-background-size-4k"] + " 1921w, " + data.acf.background_image.sizes["hero-background-size-hd"] + " 1441w, " + data.acf.background_image.sizes["hero-background-size-large"] + " 769w, " + data.acf.background_image.sizes["hero-background-size-tablet"] + " 415w, " + data.acf.background_image.sizes["hero-background-size-phablet"] + " 320w";
+        aboutText.innerHTML = content.rendered;
+        aboutBackgroundImage.srcset = acf.background_image.sizes["hero-background-size-4k"] + " 1921w, " + acf.background_image.sizes["hero-background-size-hd"] + " 1441w, " + acf.background_image.sizes["hero-background-size-large"] + " 769w, " + acf.background_image.sizes["hero-background-size-tablet"] + " 415w, " + acf.background_image.sizes["hero-background-size-phablet"] + " 320w";
         //Checks if Profile Picture has been set, then renders it
-        if (data.acf.profile_picture) {
+        if (acf.profile_picture) {
             let profilePicture = document.createElement("IMG");
             profilePicture.className = "about-image";
             infoPanelContent.prepend(profilePicture);
-            profilePicture.src = data.acf.profile_picture.sizes.thumbnail;
+            profilePicture.src = acf.profile_picture.sizes.thumbnail;
         }
         setUnderline(activeTab, tabUnderline);
         const removeActiveClass = (actives) => {
