d187f868cd8eddba8a58e40e9d5a1222ee78f625
Nick <z.nick00@gmail.com>
2019-01-13 20:32:12 -0500

    Fixes scroll event for mobile

--- src/js/public/global/variables.js
+++ src/js/public/global/variables.js
@@ -4,3 +4,14 @@ export const mobileMenuButton = document.getElementById('mobile-menu-button');
 export const loader = document.getElementById('loader');
 
 export let windowHeight = window.innerHeight;
+
+export const breakpoints = {
+    laptop: window.matchMedia("(max-width: 992px)"),
+    tablet: window.matchMedia("(max-width: 768px)"),
+    phablet: window.matchMedia("(max-width: 575px)")
+};
+export const breakpointsInverse = {
+    laptop: window.matchMedia("(min-width: 991px)"),
+    tablet: window.matchMedia("(min-width: 767px)"),
+    phablet: window.matchMedia("(min-width: 574px)")
+};
