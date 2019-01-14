079c35de12fe5d9a2e25dbe3355e0d1d9d1421b4
Nick <z.nick00@gmail.com>
2019-01-13 20:00:02 -0500

    Refactors about section AJAX functions

--- src/js/public/about/variables.js
+++ src/js/public/about/variables.js
@@ -1,5 +1,4 @@
 export const aboutTitle = document.getElementById('about-title');
-export const aboutText = document.getElementById('about-text');
 export const infoPanelContent = document.querySelector('#about .info-panel-content');
 export const aboutBackgroundImage = document.getElementById('about-background');
 export const tabUnderline = document.getElementById('underline');
